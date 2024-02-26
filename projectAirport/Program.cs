4e4d4bc4bae83a84ea8cdbfd54a2f61ae33eedc0
pietraldo <pietrzakjakub4321@gmail.com>
2024-02-26 10:56:24 +0100

    one class implemented, reading from file, serialization, deserialization

--- projectAirport/Program.cs
+++ projectAirport/Program.cs
@@ -1,12 +1,101 @@
-﻿using System;
+﻿using projectAirport;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        static void SerializeJson(List<Crew> lista)
+        {
+            string fileName = "JsonSerialization.json";
+            string jsonString = "";
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+            };
+            foreach (Crew cr in lista)
+            {
+                jsonString += JsonSerializer.Serialize(cr, options)+"\n";
+            }
+           
+            File.WriteAllText(fileName, jsonString);
+        }
+        static void DeserializeJson(string filePath)
+        {
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                // Read and display lines from the file until the end of the file is reached
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    Crew? cr = JsonSerializer.Deserialize<Crew>(line)!;
+                }
+            }
+
+        }
+        static void SerializeXML(List<Crew> lista)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Crew>));
+            using (TextWriter writer = new StreamWriter("people.xml"))
+            {
+                serializer.Serialize(writer, lista);
+            }
+
+
+        }
+       
+        static void DeserializeXML(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Crew>));
+            using (TextReader reader = new StreamReader(filePath))
+            {
+                var people = (List<Crew>)serializer.Deserialize(reader);
+
+            }
+
+        }
+        static void ReadFile(string filePath, char fieldSeparator=',')
+        {
+            if (File.Exists(filePath))
+            {
+                List<Crew> crews = new List<Crew>();
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of the file is reached
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] separated=line.Split(fieldSeparator);
+                        if (separated[0] != "C") continue;
+
+                        Crew cr = new Crew(separated);
+                        crews.Add(cr);
+                        Console.WriteLine(line);
+                    }
+                }
+                SerializeJson(crews);
+                SerializeXML(crews);
+            }
+            else
+            {
+                Console.WriteLine("File not found: " + filePath);
+                Environment.Exit(1);
+            }
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ReadFile("example_data.ftr");
+            DeserializeJson("JsonSerialization.json");
+            DeserializeXML("people.xml");
         }
     }
 }
---
+++ projectAirport/Thing.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectAirport
+{
+    public class Thing
+    {
+        public UInt64 ID { get; set; }
+
+        virtual public void UploadFromFile() { }
+    }
+
+    [Serializable]
+    public class Crew:Thing
+    {
+        public string Name { get; set; }
+        public UInt64 Age { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public UInt16 Practice { get; set; }
+        public string Role { get; set; }
+
+        public Crew(){}
+
+        public Crew(UInt64 ID,string name, ulong age, string phone, string email, ushort practice, string role)
+        {
+            this.ID = ID;
+            Name = name;
+            Age = age;
+            Phone = phone;
+            Email = email;
+            Practice = practice;
+            Role = role;
+        }
+        public Crew(string[] fields)
+        {
+            if (fields.Length < 8) throw new Exception("Too short array");
+            ID = UInt64.Parse(fields[1]);
+            Name = fields[2];
+            Age = UInt64.Parse(fields[3]);
+            Phone = fields[4];
+            Email = fields[5];
+            Practice = UInt16.Parse(fields[6]);
+            Role = fields[7];
+        }
+    }
+}
+
+// kazda klasa musi miec: serializer i tworzenie ze stringa
