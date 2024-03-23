b129cd086c6fa52119ff773d9158640cd7887480
pietraldo <pietrzakjakub4321@gmail.com>
2024-03-23 19:54:17 +0100

    good rotation

--- projectAirport/AdapterFlightGui.cs
+++ projectAirport/AdapterFlightGui.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mapsui.Projections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,14 +17,22 @@ namespace projectAirport
             this.flight = flight;
             
             ID = flight.ID;
-
-            float distY = end.Latitude - start.Latitude;
-            float distX= end.Longitude - start.Longitude;
-
-            //if (!(distY > 0 && distX > 0))
-            //    return;
-            MapCoordRotation = double.Pi/2- Math.Atan(distX/distY);
             WorldPosition = new WorldPosition((double)flight.Latitude, (double)flight.Longitude);
+
+            (double, double) aa = SphericalMercator.FromLonLat(start.Longitude, start.Latitude);
+            (double, double) bb = SphericalMercator.FromLonLat(end.Longitude, end.Latitude);
+
+
+
+            double distX = bb.Item1 - aa.Item1;
+            double distY = bb.Item2 - aa.Item2;
+            
+            MapCoordRotation = Math.Atan2(distX, distY);
+
+            if (!(distY > 0 && distX > 0))
+                return;
+            
+            
 
             //else if(distY>0 && distX<0)
             //    MapCoordRotation = 3*double.Pi/2- Math.Atan(Math.Abs(distX) / Math.Abs(distY));
