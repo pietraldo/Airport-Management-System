440c2579909c8636a7baed6ed646ca09ca95f549
pietraldo <pietrzakjakub4321@gmail.com>
2024-03-30 16:28:14 +0100

    small change in data source

--- projectAirport/DataSource.cs
+++ projectAirport/DataSource.cs
@@ -14,39 +14,38 @@ namespace projectAirport
         public List<Thing> thingList = new List<Thing>();
         public ListDivider divider = new ListDivider();
 
-        public void FromNetwork(string file_path) 
+        public void FromNetwork(string file_path, int min_time, int max_time) 
         {
             // starting Network Simulator in new thread
-            Thread netSim = new Thread(() => NetworkSimulator(file_path));
+            Thread netSim = new Thread(() => {
+
+                // creating network simulator
+                NetworkSourceSimulator netSim = new NetworkSourceSimulator(file_path, min_time, max_time);
+
+                // adding event handler
+                ReadNetwork reader1 = new ReadNetwork(thingList, divider);
+                netSim.OnNewDataReady += reader1.MessageHandler;
+
+                // starting serwer thread
+                Thread tcpSerwer = new Thread(new ThreadStart(netSim.Run)) { IsBackground = true };
+                tcpSerwer.Start();
+
+                // reading user commends
+                string? asw;
+                while ((asw = Console.ReadLine()) != "exit")
+                {
+                    if (asw == "print")
+                    {
+                        reader1.MakeSnapshot();
+                    }
+                }
+
+            });
             netSim.Start();
         }
         public void FromFile(string file_path) 
         {
             thingList = ReadFile.ConvertToObjects(ReadFile.ReadFileMethod(file_path), divider);
-        }
-
-        private void NetworkSimulator(string file_path)
-        {
-            // creating network simulator
-            NetworkSourceSimulator netSim = new NetworkSourceSimulator(file_path, 1, 1);
-
-            // adding event handler
-            ReadNetwork reader1 = new ReadNetwork(thingList, divider);
-            netSim.OnNewDataReady += reader1.MessageHandler;
-
-            // starting serwer thread
-            Thread tcpSerwer = new Thread(new ThreadStart(netSim.Run)) { IsBackground = true };
-            tcpSerwer.Start();
-
-            // reading user commends
-            string? asw;
-            while ((asw = Console.ReadLine()) != "exit")
-            {
-                if (asw == "print")
-                {
-                    reader1.MakeSnapshot();
-                }
-            }
         }
     }
 }
