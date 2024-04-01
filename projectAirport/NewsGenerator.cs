af9dad6493ab21717c66d89a76b4c3906ec2665f
pietraldo <pietrzakjakub4321@gmail.com>
2024-04-01 19:07:16 +0200

    added iterator

--- projectAirport/NewsGenerator.cs
+++ projectAirport/NewsGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,84 @@ using System.Threading.Tasks;
 
 namespace projectAirport
 {
-    public class NewsGenerator(List<Media> medias, List<IReportable> reportables)
+    public class NewsGenerator
     {
-        private List<Media> _media=medias;
-        private List<IReportable> _reportabl=reportables;
-       
-        public string GenerateNextNews()
+        private List<Media> _media;
+        private List<IReportable> _reportable;
+        private AllCombinationIterator _allCombinationIterator;
+
+        public NewsGenerator(List<Media> media, List<IReportable> reportabl)
         {
-            return "";
+            _media = media;
+            _reportable = reportabl;
+            _allCombinationIterator = new AllCombinationIterator(_media, _reportable);
+        }
+
+        public string? GenerateNextNews()
+        {
+            if(_allCombinationIterator.MoveNext())
+            {
+                (Media mediaObj, IReportable reporatbleObj) = _allCombinationIterator.Current;
+                return mediaObj.Report(reporatbleObj);
+            }
+            return null;
+        }
+
+        private class AllCombinationIterator : IEnumerator<(Media, IReportable)>
+        {
+            private List<Media> _media;
+            private List<IReportable> _reportable;
+            private IEnumerator<IReportable> _repEnumerator;
+            private IEnumerator<Media> _mediaEnumerator;
+
+            public AllCombinationIterator(List<Media> media, List<IReportable> reportable)
+            {
+                _media = media;
+                _reportable = reportable;
+                Reset();
+            }
+
+            public bool MoveNext()
+            {
+                if(!_repEnumerator.MoveNext())
+                {
+                    _repEnumerator.Reset();
+                    _repEnumerator.MoveNext();
+                    if (!_mediaEnumerator.MoveNext())
+                    {
+                        Reset();
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            public void Reset()
+            {
+                _mediaEnumerator= _media.GetEnumerator();
+                _repEnumerator=_reportable.GetEnumerator();
+                _mediaEnumerator.MoveNext();
+            }
+            public void Dispose()
+            {
+                _mediaEnumerator.Dispose();
+                _repEnumerator.Dispose();
+            }
+
+            object IEnumerator.Current => Current;
+
+            public (Media, IReportable) Current
+            {
+                get
+                {
+                    if (_repEnumerator == null || _mediaEnumerator == null)
+                        throw new InvalidOperationException("Enumerator is not initialized.");
+                    return new (_mediaEnumerator.Current, _repEnumerator.Current);
+                }
+            }
+
         }
     }
+
+    
 }
--- projectAirport/Program.cs
+++ projectAirport/Program.cs
@@ -26,24 +26,30 @@ namespace projectAirport
             DataSource dataSource = new DataSource();
             dataSource.FromFile("data/example_data.ftr");
 
-            Television abelowa = new Television("Abelowa");
-            Radio shmen = new Radio("Shmen");
+            // creating medias
+            List<Media> media = new List<Media>()
+            {
+                new Television("Telewizja Abelowa"),
+                new Television("Kanał TV-tensor"),
+                new Radio("Radio Kwantyfikator"),
+                new Radio("Radio Shmen"),
+                new Newspaper("Gazeta Kategoryczna"),
+                new Newspaper("Dziennik Polityczny")
+            };
 
-            List<Media> media = new List<Media>();
-            media.Add(abelowa);
-            media.Add(shmen);
 
             List<IReportable> reportables = new List<IReportable>();
             reportables.Add(dataSource.divider.Airports.First());
             reportables.Add(dataSource.divider.PassengerPlanes.First());
 
-            foreach (Media mediaItem in media)
+            NewsGenerator newsGenerator = new NewsGenerator(media, reportables);
+            string? news;
+            while ((news = newsGenerator.GenerateNextNews()) != null)
             {
-                foreach(IReportable ir in reportables)
-                {
-                    Console.WriteLine(mediaItem.Report(ir));
-                }
+                Console.WriteLine(news);
             }
+
+
 
         }
     }
